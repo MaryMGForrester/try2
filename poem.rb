6f08d9cce4064ddea1045a9c30e2ba618100c684
MaryMGForrester <marymgfors9@gmail.com>
2024-09-03 22:16:48 +0300

    описание изменений

--- poem.rb
+++ poem.rb
@@ -1,1 +1,6 @@
-poem = 'My toast has flown from my hand and toast has gone to the moon'
+poem = "My toast has flown from my hand
+And my toast has gone to the moon.
+But when I saw it on television,
+Planting our flag on Halley's comet,
+More still did I want to eat it"
+puts poem.lines.reverse
